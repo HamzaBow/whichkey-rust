0e359ea87b6fceb7d6d0fb7e03e9fea425d96162
Hamza Bow <hamzabow77@gmail.com>
2023-11-16 23:36:27 +0100

    add 'new' contructor to Node's struct

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,13 @@ struct Node {
     description: String,
 }
 
-fn main() {}
+impl Node {
+    fn new(name: String, description: String) -> Self {
+        Self { name, description }
+    }
+}
+
+fn main() {
+    let node1 = Node::new("Root".to_string(), "".to_string());
+    let node2 = Node::from(node1);
+}
