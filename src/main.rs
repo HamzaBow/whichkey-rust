e71e4a77e597501939513c2df7275d3dbaa8f0cf
Hamza Bow <hamzabow77@gmail.com>
2023-11-18 00:44:53 +0100

    test adding a second child node in a different way

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,21 @@ use node::Node;
 
 fn main() {
     let mut node = Node::new("Root".to_string(), "".to_string());
+    let child_node = Node::new("Child A".to_string(), "".to_string());
+    node.children.insert('a', child_node);
     node.children
-        .insert('a', Node::new("Child A".to_string(), "".to_string()));
+        .insert('b', Node::new("Child B".to_string(), "".to_string()));
     println!("{}", node);
-    let child = node.get_child('a');
+    let child_a = node.get_child('a');
+    let child_b = node.get_child('b');
 
-    match child {
-        Some(child) => println!("{}", child),
+    print_node_if_exists(child_a);
+    print_node_if_exists(child_b);
+}
+
+fn print_node_if_exists(possible_node: Option<&Node>) {
+    match possible_node {
+        Some(node) => println!("{}", node),
         None => println!("No child found"),
     }
 }
