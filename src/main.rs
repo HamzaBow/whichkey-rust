cb18cbd4d59a5ab11170b3bcbfb9ad2d64fb5e99
Hamza Bow <hamzabow77@gmail.com>
2023-11-18 00:35:46 +0100

    use 'Node' directly without prefixing with the mode name

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 mod node;
+use node::Node;
 
 fn main() {
-    let mut node = node::Node::new("Root".to_string(), "".to_string());
+    let mut node = Node::new("Root".to_string(), "".to_string());
     node.children
-        .insert('a', node::Node::new("Child A".to_string(), "".to_string()));
+        .insert('a', Node::new("Child A".to_string(), "".to_string()));
     println!("{}", node);
     let child = node.get_child('a');
 
