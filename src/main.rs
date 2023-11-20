988b96ee5c474d16b1d763861fc89d3f3c001e77
Hamza Bow <hamzabow77@gmail.com>
2023-11-20 22:48:47 +0100

    run command from node

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod node;
+use std::process::Command;
+
 use node::Node;
 
 fn main() {
-    let node = Node::new("Root".to_string(), "".to_string(), None);
-    println!("{:#?}", node)
+    let command = Command::new("microsoft-edge");
+    let node = Node::new("Root".to_string(), "".to_string(), Some(command));
+    // println!("{:#?}", node)
+    let res = node.command.unwrap().output();
 }
