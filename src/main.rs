69076a2ac6457797c770aef7527b265d13e2d1f7
Hamza Bow <hamzabow77@gmail.com>
2023-11-20 22:42:29 +0100

    remove more boiler plate

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,6 @@ mod node;
 use node::Node;
 
 fn main() {
-    let mut node = Node::new("Root".to_string(), "".to_string(), None);
-    let child_node = Node::new("Child A".to_string(), "".to_string(), None);
-    node.children.insert('a', child_node);
-    node.children
-        .insert('b', Node::new("Child B".to_string(), "".to_string(), None));
-    node['c'] = Node::new("Child c".to_string(), "".to_string(), None);
-    println!("{:#?}", node);
+    let node = Node::new("Root".to_string(), "".to_string(), None);
+    println!("{:#?}", node)
 }
-
-// fn print_node_if_exists(possible_node: Option<&Node>) {
-//     match possible_node {
-//         Some(node) => println!("{}", node),
-//         None => println!("No child found"),
-//     }
-// }
